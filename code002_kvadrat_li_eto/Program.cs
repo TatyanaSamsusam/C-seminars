f53d29b922e23716fe99c7fcae065699631ab099
Tatyana <android87@list.ru>
2022-03-02 17:32:22 +0300

    second try

---
+++ code002_kvadrat_li_eto/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("type numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type any numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB == numberA * numberA)
+{
+    Console.WriteLine ("True");
+    }
+else 
+{
+    Console.WriteLine ("False");
+}
