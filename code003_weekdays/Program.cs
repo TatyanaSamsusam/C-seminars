7935b57e4e1951c3d31cb6f9bae7c3cc3edfe829
Tatyana <android87@list.ru>
2022-03-02 18:08:47 +0300

    made weekdays using switch

--- code003_weekdays/Program.cs
+++ code003_weekdays/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Days = new int ();
+
+Console.WriteLine ("type a number");
+Days = Convert.ToInt32(Console.ReadLine());
+
+switch (Days)
+{
+    case 1:
+    {
+        Console.WriteLine("the first day");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("the second day");
+        break;
+    }
+        case 3:
+    {
+        Console.WriteLine("the third day");
+        break;
+    }
+        case 4:
+    {
+        Console.WriteLine("the fourth day");
+        break;
+    }    
+        case 5:
+    {
+        Console.WriteLine("the fifth day");
+        break;
+    }
+        case 6:
+    {
+        Console.WriteLine("the sixth day");
+        break;
+    }
+        case 7:
+    {
+        Console.WriteLine("the seventh day");
+        break;
+    }
+}
