72c257c0f8338ef05ccd0f46308c97fb0c6a624c
Tatyana <android87@list.ru>
2022-03-11 15:05:56 +0300

    added find random and get max number

---
+++ code005_findRandomShowMaxNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int GetMaxLetterOfNumber (int number)
+{
+    int result = number % 10;
+    int letter2 = number / 10;
+    if (result < letter2)
+    {
+        result = letter2;
+    }
+    return result;
+}
+
+int RandNumber = new Random() .Next(10,99);
+int result = GetMaxLetterOfNumber(RandNumber);
+
+Console.WriteLine($"у числа {RandNumber} наибольшая цифра это {result}");
