87fffb0a260651828f9a1094bc24d22e0b92bc55
Tatyana <android87@list.ru>
2022-03-14 17:13:58 +0300

    check if A is divisible by B

---
+++ code008_kratnoe li/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("input a number A");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input a number B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine($"number A is divisible by number B");
+}
+else
+{
+    Console.WriteLine($"number A is not divisible by number B, remainder is {a % b}");
+}
