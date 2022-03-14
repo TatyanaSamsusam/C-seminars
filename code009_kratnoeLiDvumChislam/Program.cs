92e74685cfe244301e8a024c3ba8203cd854826f
Tatyana <android87@list.ru>
2022-03-14 17:23:06 +0300

    check if divisible by two numbers

---
+++ code009_kratnoeLiDvumChislam/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("\ninput a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"number is divisible by 7 and 23");
+}
+else
+{
+    Console.WriteLine($"number is NOT divisible by 7 and 23");
+}
+
