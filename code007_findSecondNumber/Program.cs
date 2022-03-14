261361943403cf4896fa82f01fb24574f4ba9038
Tatyana <android87@list.ru>
2022-03-14 16:49:10 +0300

    show second number

---
+++ code007_findSecondNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new int();
+System.Console.WriteLine("input a number from 100 till 999");
+number = Convert.ToInt32(Console.ReadLine());
+
+number = number / 10;
+int result = number % 10;
+
+Console.WriteLine($"вторая цифра это {result}");
