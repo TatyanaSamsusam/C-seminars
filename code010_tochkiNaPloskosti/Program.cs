e95e725be958c37babc15d3b8865d689490b7fae
Tatyana <android87@list.ru>
2022-03-16 15:13:42 +0300

    find a quarter

---
+++ code010_tochkiNaPloskosti/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("\ninput X and Y: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine ("the first quarter");
+}
+else if(X < 0 && Y > 0)
+{
+    Console.WriteLine ("the second quarter");
+}
+else if(X > 0 && Y < 0)
+{
+    Console.WriteLine ("the fourth quarter");
+}
+else if(X < 0 && Y < 0)
+{
+    Console.WriteLine ("the third quarter");
+}
+else
+{
+    Console.WriteLine("zero");
+}
