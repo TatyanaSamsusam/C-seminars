d96f0eed312eb386db957040b23da5760c660065
Tatyana <android87@list.ru>
2022-03-17 21:42:34 +0300

    rasstoyanie mezhdu tochkami wrong

---
+++ code012_rasstoyanieMezhduTochkami/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("\nвведите координаты точек A и B и я посчитаю расстояние между ними в 2D: ");
+int A1 = Convert.ToInt32(Console.ReadLine());
+int B1 = Convert.ToInt32(Console.ReadLine());
+int A2 = Convert.ToInt32(Console.ReadLine());
+int B2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2);
+result = Math.Round(result, 2);
+
+Console.WriteLine(result);
