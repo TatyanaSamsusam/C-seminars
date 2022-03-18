04ffe7e1aa0799f8b5c615c7693106d1b5850ed9
Tatyana <android87@list.ru>
2022-03-18 19:15:25 +0300

    made correct math.pow

--- code012_rasstoyanieMezhduTochkami/Program.cs
+++ code012_rasstoyanieMezhduTochkami/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("\nвведите координаты точек A и B и я посчитаю расстояние между ними в 2D: ");
 int A1 = Convert.ToInt32(Console.ReadLine());
-int B1 = Convert.ToInt32(Console.ReadLine());
 int A2 = Convert.ToInt32(Console.ReadLine());
+int B1 = Convert.ToInt32(Console.ReadLine());
 int B2 = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2);
+double result = Math.Sqrt (Math.Pow (A2 - A1, 2) + Math.Pow (B2 - B1, 2));
 result = Math.Round(result, 2);
 
 Console.WriteLine(result);
