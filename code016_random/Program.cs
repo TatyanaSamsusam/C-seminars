56f7c4055164c2f98d2b9b7c24c20e40b04f15be
Tatyana <android87@list.ru>
2022-03-21 21:31:24 +0300

    made random massiv

---
+++ code016_random/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+void CreateArray(int[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    Console.Write("[");
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = new int[8];
+CreateArray(arr);
+PrintArray(arr);
