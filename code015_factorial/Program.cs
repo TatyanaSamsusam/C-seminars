2d8418ed31bff410bd95e541e027e34e2e584940
Tatyana <android87@list.ru>
2022-03-21 21:12:50 +0300

    made factorial

---
+++ code015_factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+int factorial (int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int fact = factorial(num);
+System.Console.WriteLine($"факториал числа {num} равен {fact}");
