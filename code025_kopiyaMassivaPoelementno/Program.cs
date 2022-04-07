2b124b80ecba5e949a55bc52aaca364b7df3fad2
Tatyana <android87@list.ru>
2022-04-07 16:47:42 +0400

    copied array from original

---
+++ code025_kopiyaMassivaPoelementno/Program.cs
@@ -0,0 +1,28 @@
+﻿// задача 45. напишите программу, к-я будет создавать копию заданного массива с помощью поэлементного копирования
+
+int[] array = {1, 2, 3, 4, 5};
+int[] newArray = new int[array.Length];
+Console.WriteLine("1st: ");
+printArray(array);
+Console.WriteLine("2nd: ");
+newArray = Copy(array);
+printArray(newArray);
+
+int[] Copy (int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    } 
+    return result;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
